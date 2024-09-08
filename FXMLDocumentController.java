eb7b7ae1834660c8115232cd083a5b8e0e0741d6
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-09-08 13:34:47 +0200

    Update FXMLDocumentController.java
    terminando il codice con le ultime funzioni

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -45,3 +45,52 @@ public class FXMLDocumentController implements Initializable {
             default:
         }
     }
+    @FXML
+    private void handlerDigitAction(ActionEvent event) {
+        String digitObject = ((Button) event.getSource()).getText();
+        String oldText = txtDisplay.getText();
+        String newText = oldText + digitObject;
+        txtDisplay.setText(newText);
+    }
+
+    @FXML
+    private void handlerDecimalAction(ActionEvent event) {
+        if (decimalClick == 0) {
+            String decimalObject = ((Button) event.getSource()).getText();
+            String oldText = txtDisplay.getText();
+            System.out.println(oldText);
+            String newText = oldText + decimalObject;
+            System.out.println(newText);
+            txtDisplay.setText(newText);
+            decimalClick = 1;
+        }
+    }
+
+    @FXML
+    private void handlerEqualAction(ActionEvent event) {
+        double secondDouble;
+        double result=0;
+        String secondText = txtDisplay.getText();
+        secondDouble = Double.parseDouble(secondText);
+
+        switch (generalOperationObject) {
+            case "+":
+                result = firstDouble + secondDouble;
+                break;
+            case "-":
+                result = firstDouble - secondDouble;
+                break;
+            case "*":
+                result = firstDouble * secondDouble;
+                break;
+            case "/":
+                result = firstDouble / secondDouble;
+                break;
+            default:
+        }
+        String format = String.format("%.1f", result);
+        txtDisplay.setText(format);
+
+    }
+
+}
