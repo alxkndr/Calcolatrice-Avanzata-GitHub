53fc2abb9b57bc3d5aaa7ee98dee3e116e992c67
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-09-08 13:32:42 +0200

    Update FXMLDocumentController.java
    import

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1,1 +1,7 @@
-
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
