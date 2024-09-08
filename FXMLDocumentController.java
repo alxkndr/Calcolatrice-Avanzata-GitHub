e6899b1da91d9b5922ded3e038e1aef2555c31db
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-09-08 13:33:39 +0200

    Update FXMLDocumentController.java
    public class con alcuni dei FXML

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -5,3 +5,43 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private TextField txtDisplay;
+    private int decimalClick = 0;
+    private String generalOperationObject;
+    private double firstDouble;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void handlerGeneralAction(ActionEvent event) {
+        generalOperationObject = ((Button) event.getSource()).getText();
+        switch (generalOperationObject) {
+            case "AC":
+                txtDisplay.setText("");
+                decimalClick = 0;
+                break;
+            case "+/-":
+                double plusMinus = Double.parseDouble(String.valueOf(txtDisplay.getText()));
+                plusMinus = plusMinus * (-1);
+                txtDisplay.setText(String.valueOf(plusMinus));
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "%":
+                String currentText = txtDisplay.getText();
+                firstDouble = Double.parseDouble(currentText);
+                txtDisplay.setText("");
+                decimalClick = 0;
+                break;
+            default:
+        }
+    }
