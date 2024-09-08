6aca501c37c5d8135791a0504ede2f23ca3aa478
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-09-08 13:36:07 +0200

    Update MainClass.java
    public class per avviare il programma della calcolatrice

--- MainClass.java
+++ MainClass.java
@@ -4,3 +4,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+public class MainClass extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("calculator.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
